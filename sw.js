b5703a015d3572306314485e2521af1ce7cef9ac
zokn007 <gz4sbmypky@privaterelay.appleid.com>
2026-07-15 13:48:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='moj-avto-v1.3-2';
+const FILES=['./','./index.html','./manifest.json','./icon.svg','./peugeot-508-sw.jpg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
