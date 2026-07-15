5efee0ed729f695f4eded86b74939a28e289727e
zokn007 <gz4sbmypky@privaterelay.appleid.com>
2026-07-15 15:59:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='moj-avto-v1.3-2';
-const FILES=['./','./index.html','./manifest.json','./icon.svg','./peugeot-508-sw.jpg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='moj-avto-v1-4-3';const FILES=['./','./index.html','./manifest.json','./icon.png','./dg-smart-apps.png','./peugeot-508-sw.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
